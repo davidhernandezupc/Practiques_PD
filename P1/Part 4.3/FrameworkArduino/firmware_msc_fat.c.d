038b9880ec957f409267443d71c0571a9d7d929c
davidhernandezupc <162739606+davidhernandezupc@users.noreply.github.com>
2025-03-14 20:22:54 +0100

    Practica1 -> 4.3 actualizada

--- P1/Part 4.3/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3-devkitm-1/FrameworkArduino/firmware_msc_fat.c.o: \
- C:/Users/Student/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- C:/Users/Student/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
